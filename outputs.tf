17eca10eb6c298fd3a6ecdd0d78847b74c5a0abf
Jillian <jillian.e.rowe@gmail.com>
2021-08-08 08:36:55 -0400

    update readme

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,19 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
+
+output "aws_batch_job_queue" {
+  value = aws_batch_job_queue.default_queue.name
+}
+
+output "aws_batch_compute_environment" {
+  value = aws_batch_compute_environment.batch
+}
+
+output "aws_iam_role-batch_secrets_role" {
+  value = aws_iam_role.batch_secrets_role
+}
+
+output "aws_secrets_manager_secret-batch" {
+  value = aws_secretsmanager_secret.batch 
+}
