9af0cee1bdd40715e256e63ba64c6e0a579bbbad
Jillian <jillian.e.rowe@gmail.com>
2022-05-05 19:51:51 +0300

    updating aws ecs ami

--- modules/aws-batch-ec2/main.tf
+++ modules/aws-batch-ec2/main.tf
@@ -6,8 +6,7 @@ data "aws_ami" "ecs_latest" {
 
   filter {
     name   = "name"
-    #    values = ["amzn-ami-*-amazon-ecs-optimized"]
-    values = ["amzn2-ami-ecs-hvm-2.0.*-x86_64-eb"]
+    values = ["amzn2-ami-ecs-hvm*ebs"]
   }
 
   filter {
